2a931969bb40bf9f2720ad9a85d6eea549033d8b
Anandan <44726326+vijay043004@users.noreply.github.com>
2024-12-09 10:21:18 +0100

    Add files via upload

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Materiel;
+use App\Form\ProduitsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProduitsController extends AbstractController
 {
     #[Route('/produits', name: 'app_produits')]
-    public function index(): Response
+
+    public function new(Request $request): Response
     {
+        $materiel = new Materiel();
+        $materiel->setDateAchat(new \DateTime('now'));
+        $form = $this->createForm(ProduitsType::class, $materiel);
+
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+        }
+        
         return $this->render('produits/index.html.twig', [
             'controller_name' => 'ProduitsController',
+            'formProduits' => $form
         ]);
     }
+
 }
